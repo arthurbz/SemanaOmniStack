319448d322a3f3019168d91c5a6d95de99b8fa1f
arthurziero <39162820+arthurziero@users.noreply.github.com>
2019-10-12 18:54:04 -0300

    Mobile
    Working except for the image.

--- mobile/src/pages/Login.js
+++ mobile/src/pages/Login.js
@@ -1,11 +1,36 @@
-import React from 'react';
-import { View, StyleSheet, Image, Text, TextInput } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, AsyncStorage, StyleSheet, KeyboardAvoidingView, Image, Text, TouchableOpacity, TextInput } from 'react-native';
 
+import api from '../services/api';
 import logo from '../assets/logo.png';
 
-export default function Login() {
+export default function Login({ navigation }) {
+    const [email, setEmail] = useState('');
+    const [techs, setTechs] = useState('');
+
+    useEffect(() => {
+        AsyncStorage.getItem('user').then(user => {
+            if (user) {
+                navigation.navigate('List')
+            }
+        })
+    }, [])
+
+    async function handleSubmit() {
+        const response = await api.post('/sessions', {
+            email
+        })
+
+        const { _id } = response.data;
+
+        await AsyncStorage.setItem('user', _id);
+        await AsyncStorage.setItem('techs', techs);
+
+        navigation.navigate('List');
+    }
+
     return (
-        <View style={styles.container}>
+        <KeyboardAvoidingView behavior="padding" style={styles.container}>
             <Image source={logo} />
 
             <View style={styles.form}>
@@ -14,10 +39,29 @@ export default function Login() {
                     style={styles.input}
                     placeholder="Seu e-mail"
                     placeholderTextColor="#999"
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    keyboardType="email-address"
+                    value={email}
+                    onChangeText={setEmail}
                 />
+
+                <Text style={styles.label}>TECNOLOGIAS</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Tecnologias de Interesse"
+                    placeholderTextColor="#999"
+                    autoCapitalize="words"
+                    value={techs}
+                    onChangeText={setTechs}
+                />
+
+                <TouchableOpacity onPress={handleSubmit}style={styles.cancel_button}>
+                    <Text style={styles.buttonText}>Encontrar spots</Text>
+                </TouchableOpacity>
             </View>
 
-        </View>
+        </KeyboardAvoidingView>
     );
 }
 
@@ -25,7 +69,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems:'center'
+        alignItems: 'center'
     },
 
     form: {
@@ -48,6 +92,20 @@ const styles = StyleSheet.create({
         color: '#444',
         height: 44,
         marginBottom: 20,
-        borderRadius: 2
+        borderRadius: 5
     },
+
+    button: {
+        height: 42,
+        backgroundColor: '#f05a5b',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 5,
+    },
+
+    buttonText: {
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 16
+    }
 });
